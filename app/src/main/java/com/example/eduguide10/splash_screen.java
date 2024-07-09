e8148febd2620433428cd6b8e69065cf87718789
rowdy041 <sriram.laxmi3@gmail.com>
2024-07-09 07:35:33 +0530

    splash screen compelete

--- app/src/main/java/com/example/eduguide10/splash_screen.java
+++ app/src/main/java/com/example/eduguide10/splash_screen.java
@@ -1,6 +1,8 @@
 package com.example.eduguide10;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,14 @@ public class splash_screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash_screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
