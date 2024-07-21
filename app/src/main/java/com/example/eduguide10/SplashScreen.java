ea4942d4b58ffe91dde106f1e7b60cac650783e3
rowdy041 <sriram.laxmi3@gmail.com>
2024-07-21 11:00:08 +0530

    update on 21-07-24

--- app/src/main/java/com/example/eduguide10/SplashScreen.java
+++ app/src/main/java/com/example/eduguide10/SplashScreen.java
@@ -10,7 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class splash_screen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
