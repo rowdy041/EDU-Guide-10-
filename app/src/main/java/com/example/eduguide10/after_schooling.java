b3feeb370add98fefdb29157821d79c2285ec7cc
saikesav-sai <saikesav6725@gmail.com>
2024-10-06 20:33:59 +0530

    added design for inter, added some details

--- app/src/main/java/com/example/eduguide10/after_schooling.java
+++ app/src/main/java/com/example/eduguide10/after_schooling.java
@@ -1,6 +1,9 @@
 package com.example.eduguide10;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
